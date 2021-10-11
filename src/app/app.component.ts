0436c6823058a5762aede53ddf17835dad86c0dd
Marcio Coelho <marciocoelho@gmail.com>
2021-10-11 09:15:00 -0300

    Updated app.component.*

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
-import { CoelhoWalletService } from './coelho-wallet.service';
+import { Component } from "@angular/core";
+
+import { CoelhoWalletService } from "./coelho-wallet.service";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html'
+	selector:    "my-app",
+	templateUrl: "./app.component.html"
 })
 export class AppComponent {
-  constructor(public wallet: CoelhoWalletService) {
-  }
+	constructor(public wallet: CoelhoWalletService) {
+	}
 }
