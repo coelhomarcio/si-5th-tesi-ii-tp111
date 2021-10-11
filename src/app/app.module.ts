a8ef0878518620a4c3d62bc8aa2b80353126ca24
Marcio Coelho <marciocoelho@gmail.com>
2021-10-11 09:14:41 -0300

    Updated app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,33 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule }         from "@angular/core";
+import { BrowserModule }    from "@angular/platform-browser";
+import { RouterModule }     from "@angular/router";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
-import { CoelhoCurrencyComponent } from './coelho-currency/coelho-currency.component';
-import { CoelhoWalletComponent } from './coelho-wallet/coelho-wallet.component';
-import { HttpClientModule } from '@angular/common/http';
-import { CoelhoWalletService } from './coelho-wallet.service';
+import { AppComponent }            from "./app.component";
+import { CoelhoCurrencyComponent } from "./coelho-currency/coelho-currency.component";
+import { CoelhoWalletComponent }   from "./coelho-wallet/coelho-wallet.component";
+
+import { CoelhoWalletService } from "./coelho-wallet.service";
 
 @NgModule({
-  imports:      [
-    BrowserModule,
-    FormsModule,
-    RouterModule.forRoot(
-      [
-        { path: '', component: CoelhoCurrencyComponent },
-        { path: 'currency', component: CoelhoCurrencyComponent },
-        { path: 'wallet', component: CoelhoWalletComponent }
-      ]
-    ),
-    HttpClientModule
-  ],
-  declarations: [
-    AppComponent,
-    CoelhoCurrencyComponent,
-    CoelhoWalletComponent
-  ],
-  bootstrap:    [ AppComponent ],
-  providers: [ CoelhoWalletService ]
+	imports:      [
+		BrowserModule,
+		RouterModule.forRoot(
+			[
+				{ path: "", redirectTo: "currency", pathMatch: "full" },
+				{ path: "currency", component: CoelhoCurrencyComponent },
+				{ path: "wallet", component: CoelhoWalletComponent }
+			]
+		),
+		HttpClientModule
+	],
+	declarations: [
+		AppComponent,
+		CoelhoCurrencyComponent,
+		CoelhoWalletComponent
+	],
+	bootstrap:    [AppComponent],
+	providers:    [CoelhoWalletService]
 })
 export class AppModule {
 }
