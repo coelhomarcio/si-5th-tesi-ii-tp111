34e65c0e1054500cfc6074d0bf971c371f7cc824
Marcio Coelho <marciocoelho@gmail.com>
2021-10-11 09:16:19 -0300

    Updated coelho-wallet.service.ts

--- src/app/coelho-wallet.service.ts
+++ src/app/coelho-wallet.service.ts
@@ -1,5 +1,5 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 interface API {
 	time: { updatedISO: string };
@@ -10,94 +10,85 @@ interface API {
 	};
 }
 
-export interface Currency {
-  timeISO: string;
-  BRL: number;
-  USD: number;
-  EUR: number;
+interface Currency {
+	timeISO: string;
+	BRL: number;
+	USD: number;
+	EUR: number;
 }
 
 @Injectable()
 export class CoelhoWalletService {
-  public currencies: Currency[] = [];
-  public lastCurrency: Currency = {
-    timeISO: "", BRL: 0, USD: 0, EUR: 0
-  };
-  private urlBRL = "http://api.coindesk.com/v1/bpi/currentprice/BRL.json";
-  private urlUSDEUR = "http://api.coindesk.com/v1/bpi/currentprice.json";
-  btcBalance = 0;
+	public currencies: Currency[] = [];
+	private lastCurrency: Currency = { timeISO: "", BRL: 0, USD: 0, EUR: 0 };
+	private readonly urlBRL = "https://api.coindesk.com/v1/bpi/currentprice/BRL.json";
+	private readonly urlUSDEUR = "https://api.coindesk.com/v1/bpi/currentprice.json";
+	public btcBalance = 0;
 
-  constructor(private http: HttpClient) {
-    this.fetchCurrencies().then();
-    setInterval(() => this.fetchCurrencies().then(), 60000);
-  }
+	constructor(private http: HttpClient) {
+		this.fetchCurrencies().then();
+		setInterval(() => this.fetchCurrencies().then(), 60000);
+	}
 
-  public async fetchCurrencies() {
-    const dataBRL = await this.http.get<API>(this.urlBRL).toPromise();
-    const dataUSDEUR = await this.http.get<API>(this.urlUSDEUR).toPromise();
-    this.lastCurrency.timeISO = dataBRL.time.updatedISO;
-    this.lastCurrency.BRL = dataBRL.bpi.BRL.rate_float;
-    this.lastCurrency.USD = dataUSDEUR.bpi.USD.rate_float;
-    this.lastCurrency.EUR = dataUSDEUR.bpi.EUR.rate_float;
-    const obj = Object.assign({}, this.lastCurrency);
-    this.updateCurrencies(obj);
-  }
+	private async fetchCurrencies() {
+		const dataBRL = await this.http.get<API>(this.urlBRL).toPromise();
+		const dataUSDEUR = await this.http.get<API>(this.urlUSDEUR).toPromise();
+		this.lastCurrency.timeISO = dataBRL.time.updatedISO;
+		this.lastCurrency.BRL = dataBRL.bpi.BRL.rate_float;
+		this.lastCurrency.USD = dataUSDEUR.bpi.USD.rate_float;
+		this.lastCurrency.EUR = dataUSDEUR.bpi.EUR.rate_float;
+		const obj = Object.assign({}, this.lastCurrency);
+		this.updateCurrencies(obj);
+	}
 
-  private updateCurrencies(obj: Currency) {
-    this.currencies.push(obj);
-  }
+	private updateCurrencies(obj: Currency) {
+		this.currencies.push(obj);
+	}
 
-  public brlBalance() {
-    const length = this.currencies.length;
-    if (length > 0) {
-      return this.btcBalance * this.lastCurrency.BRL;
-    }
-    return 0;
-  }
+	public brlBalance() {
+		const length = this.currencies.length;
+		if (length > 0)
+			return this.btcBalance * this.lastCurrency.BRL;
+		return 0;
+	}
 
-  public usdBalance() {
-    const length = this.currencies.length;
-    if (length > 0) {
-      return this.btcBalance * this.lastCurrency.USD;
-    }
-    return 0;
-  }
+	public usdBalance() {
+		const length = this.currencies.length;
+		if (length > 0)
+			return this.btcBalance * this.lastCurrency.USD;
+		return 0;
+	}
 
-  public eurBalance() {
-    const length = this.currencies.length;
-    if (length > 0) {
-      return this.btcBalance * this.lastCurrency.EUR;
-    }
-    return 0;
-  }
+	public eurBalance() {
+		const length = this.currencies.length;
+		if (length > 0)
+			return this.btcBalance * this.lastCurrency.EUR;
+		return 0;
+	}
 
-  public addBTCBalance(value: number) {
-    const length = this.currencies.length;
-    if (length > 0) {
-      this.btcBalance += value / this.lastCurrency.BRL;
-    }
-  }
+	public depositBTC(value: number) {
+		const length = this.currencies.length;
+		if (length > 0)
+			this.btcBalance += value / this.lastCurrency.BRL;
+	}
 
-  public removeBTCBalance(value: number) {
-    const length = this.currencies.length;
-    if (length > 0) {
-      this.btcBalance -= value / this.lastCurrency.BRL;
-    }
-  }
+	public withdrawBTC(value: number) {
+		const length = this.currencies.length;
+		if (length > 0)
+			this.btcBalance -= value / this.lastCurrency.BRL;
+	}
 
-  public isUp() {
-    const length = this.currencies.length;
-    if (length > 1) {
-      return this.lastCurrency.BRL > this.currencies[length - 2].BRL;
-    }
-    return false;
-  }
+	public isUp() {
+		const length = this.currencies.length;
+		if (length > 1)
+			return this.lastCurrency.BRL > this.currencies[length - 2].BRL;
+		return false;
+	}
 
-  public isDown() {
-    const length = this.currencies.length;
-    if (length > 1) {
-      return this.lastCurrency.BRL < this.currencies[length - 2].BRL;
-    }
-    return false;
-  }
+	public isDown() {
+		const length = this.currencies.length;
+		if (length > 1)
+			return this.lastCurrency.BRL < this.currencies[length - 2].BRL;
+		return false;
+	}
 }
