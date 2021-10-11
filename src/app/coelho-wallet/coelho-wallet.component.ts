1d9aa6bc825f416574ad9a1e5f5d40d1c6a948a8
Marcio Coelho <marciocoelho@gmail.com>
2021-10-11 09:16:05 -0300

    Updated coelho-wallet.component.*

--- src/app/coelho-wallet/coelho-wallet.component.ts
+++ src/app/coelho-wallet/coelho-wallet.component.ts
@@ -1,14 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { CoelhoWalletService } from '../coelho-wallet.service';
+import { Component } from "@angular/core";
+
+import { CoelhoWalletService } from "../coelho-wallet.service";
 
 @Component({
-  selector: 'app-coelho-wallet',
-  templateUrl: './coelho-wallet.component.html'
+	selector:    "app-coelho-wallet",
+	templateUrl: "./coelho-wallet.component.html"
 })
-export class CoelhoWalletComponent implements OnInit {
-
-  constructor(public wallet: CoelhoWalletService) { }
-
-  ngOnInit() {
-  }
+export class CoelhoWalletComponent {
+	constructor(public wallet: CoelhoWalletService) {
+	}
 }
